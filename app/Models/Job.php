a16268551fb4b2be8f5197045e86a21029cd310a
tomislav <tomothevinkan3@gmail.com>
2024-05-19 12:39:10 +0200

    Job model

---
+++ app/Models/Job.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    use HasFactory, HasUuids;
+    public $incrementing = false;
+
+    protected $fillable = [
+        'listing_id', 'price', 'is_completed'
+    ];
+
+    public function listing() {
+        return $this->belongsTo(Listing::class);
+    }
+}
---
+++ database/migrations/2024_05_19_103501_create_jobs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->uuid('listing_id');
+            $table->double('price');
+            $table->boolean('is_completed')->default(0);
+
+            $table->foreign('listing_id')
+                ->references('id')
+                ->on('listings')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
